9920e9d49f882b9bad5e80b69b2a9a893654a979
Spaceface16518 <amrit.rathie@gmail.com>
2022-11-13 04:34:39 -0600

    use json for all endpoints

--- src/main.rs
+++ src/main.rs
@@ -36,8 +36,8 @@ fn db_fairing() -> AdHoc {
 }
 
 #[get("/alerts")]
-async fn get_alerts(mut db: Connection<Postgis>) -> String {
-    match sqlx::query(
+async fn get_alerts(mut db: Connection<Postgis>) -> Json<impl Serialize> {
+    let alerts = sqlx::query!(
         "SELECT json_build_object(
             'id',          id,
             'person_name', person_name,
@@ -56,19 +56,17 @@ async fn get_alerts(mut db: Connection<Postgis>) -> String {
     )
     .fetch_all(&mut *db)
     .await
-    {
-        Ok(data) => format!("[{}]", data
-            .iter()
-            .map(|r| format!("{}", r.get_unchecked::<String, _>(0)))
-            .collect::<Vec<String>>()
-            .join(",")),
-        _ => String::from(""),
-    }
+    .unwrap()
+    .iter()
+    .map(|row| row.json_build_object.as_ref().unwrap())
+    .cloned()
+    .collect::<Vec<_>>();
+    Json(alerts)
 }
 
 #[get("/sightings")]
-async fn get_sightings(mut db: Connection<Postgis>) -> String {
-    match sqlx::query(
+async fn get_sightings(mut db: Connection<Postgis>) -> Json<impl Serialize> {
+    let sightings = sqlx::query!(
         "SELECT json_build_object(
             'id',          id,
             'car_color',   car_color,
@@ -78,23 +76,26 @@ async fn get_sightings(mut db: Connection<Postgis>) -> String {
             'car_year',    car_year,
             'created_at',  created_at,
             'geometry',    ST_AsGeoJSON(geog)::json
-        ) FROM sightings;"
+        ) FROM sightings;",
     )
     .fetch_all(&mut *db)
     .await
-    {
-        Ok(data) => data 
-            .iter()
-            .map(|r| format!("{}", r.get_unchecked::<String, _>(0)))
-            .collect::<Vec<String>>()
-            .join(""),
-        _ => String::from("")
-    }
+    .unwrap()
+    .iter()
+    .map(|row| row.json_build_object.as_ref().unwrap())
+    .cloned()
+    .collect::<Vec<_>>();
+    Json(sightings)
 }
 
 #[get("/sightings-within-radius?<long>&<lat>&<radius>")]
-async fn get_sightings_within_radius(mut db: Connection<Postgis>, long: f64, lat: f64, radius: f64) -> Json<impl Serialize> {
-    let records =  sqlx::query!(
+async fn get_sightings_within_radius(
+    mut db: Connection<Postgis>,
+    long: f64,
+    lat: f64,
+    radius: f64,
+) -> Json<impl Serialize> {
+    let records = sqlx::query!(
         "SELECT json_build_object (
             'id',          id,
             'car_color',   car_color,
@@ -104,10 +105,14 @@ async fn get_sightings_within_radius(mut db: Connection<Postgis>, long: f64, lat
             'car_year',    car_year,
             'created_at',  created_at,
             'geometry',    ST_AsGeoJSON(geog)::json
-        ) FROM sightings WHERE ST_Distance(geog, ST_MakePoint($1 , $2)) <= $3;", long, lat, radius
+        ) FROM sightings WHERE ST_Distance(geog, ST_MakePoint($1 , $2)) <= $3;",
+        long,
+        lat,
+        radius
     )
     .fetch_all(&mut *db)
-    .await.unwrap()
+    .await
+    .unwrap()
     .iter()
     .map(|r| r.json_build_object.as_ref().unwrap())
     .cloned()
@@ -127,8 +132,8 @@ fn rocket() -> _ {
         routes![
             healthcheck,
             get_alerts,
-            get_sitings,
-            get_sitings_within_radius
+            get_sightings,
+            get_sightings_within_radius
         ],
     )
 }
